25b4bc254afb884b3c932026d784001509708055
Ken Nartey <101053439+gitdela@users.noreply.github.com>
2024-07-08 23:54:58 +0000

    rate section table

--- src/components/feature/RateSection/ratesection.tsx
+++ src/components/feature/RateSection/ratesection.tsx
@@ -15,54 +15,54 @@ export function RateSection({ rates }: Props) {
         </p>
 
         <div className='w-full'>
-          <table className='text-black w-full md:w-[80%] mx-auto mb-6'>
+          <table className='w-full md:w-[80%] mx-auto mb-6'>
             <caption className='caption-top py-4 mx-auto text-center text-sm text-muted-foreground'>
               This page displays the latest prices, 24-hour trading volume,
               price changes, and market capitalizations for all cryptocurrencies
               on Mybitstore.
             </caption>
 
-            <thead className='bg-[#2D5992]/10'>
-              <tr>
+            <thead className='bg-night-50 font-semibold'>
+              <tr className=''>
                 <th
                   scope='col'
-                  className='md:px-6 py-4 pl-2 text-left whitespace-nowrap text-white font-normal text-xs md:text-sm'
+                  className='px-4 md:px-6 text-left py-5 whitespace-nowrap text-white text-xs md:text-sm'
                 >
                   Name
                 </th>
                 <th
                   scope='col'
-                  className='md:px-6 py-4 text-right whitespace-nowrap text-white font-normal text-xs md:text-sm'
+                  className='px-2 md:px-6 text-right whitespace-nowrap text-white text-xs md:text-sm'
                 >
                   Price (USD)
                 </th>
                 <th
                   scope='col'
-                  className='md:px-6 py-4 text-right hidden md:table-cell whitespace-nowrap text-white font-normal text-xs md:text-sm'
+                  className='px-2 md:px-6 text-right hidden md:table-cell whitespace-nowrap text-white text-xs md:text-sm'
                 >
                   Last Price
                 </th>
                 <th
                   scope='col'
-                  className='md:px-6 py-4 pe-2 text-right md:table-cell text-white font-normal text-xs md:text-sm'
+                  className='px-4 md:px-6 text-right md:table-cell text-white text-xs md:text-sm'
                 >
                   <span className='hidden md:block'>24h % Change</span>
                   <span className='md:hidden'>24h % Chg</span>
                 </th>
                 {/* <th
                   scope='col'
-                  className='md:px-6 py-4 text-right hidden md:table-cell whitespace-nowrap text-white font-normal text-xs md:text-sm'
+                  className='md:px-6 text-right hidden md:table-cell whitespace-nowrap text-white font-normal text-xs md:text-sm'
                 >
                   Action
                 </th> */}
               </tr>
             </thead>
 
-            <tbody className='md:divide-y divide-[#2D5992]/20 border-[#2D5992]'>
+            <tbody className='md:divide-y divide-[#2D5992]/20 border-[#2D5992] py-8'>
               {rates.length > 0 &&
                 rates.map((value: any, index: any) => (
                   <tr key={index} className='cursor-pointer'>
-                    <td className='px-2 md:px-6 py-4 whitespace-nowrap text-sm text-white flex w-fit items-center gap-1 md:gap-2'>
+                    <td className='px-2 md:px-6 py-5 whitespace-nowrap text-sm text-white flex w-fit items-center gap-1 md:gap-2'>
                       <Image
                         priority
                         src={value.image}
@@ -72,10 +72,10 @@ export function RateSection({ rates }: Props) {
                         className='w-6 h-6 md:w-8 md:h-8'
                       />
                       <div className='flex flex-col items-start'>
-                        <span className='text-xs md:text-base capitalize'>
+                        <span className='text-xs md:text-base font-medium capitalize'>
                           {value.label}
                         </span>
-                        <span className='uppercase text-xs md:text-base font-light text-muted-foreground'>
+                        <span className='uppercase text-xs md:text-base font-normal text-muted-foreground'>
                           {value.ticker}
                         </span>
                       </div>
@@ -107,7 +107,7 @@ export function RateSection({ rates }: Props) {
                     </td>
                     <td className='px-2 ps-8 md:px-6 whitespace-nowrap text-right text-white'>
                       {Number(value.price_change_percentage_24h) > 0 ? (
-                        <div className='bg-[#2caf88] max-md:w-20 w-20 py-2 md:px-4 text-xs ms-auto rounded text-center font-semibold'>
+                        <div className='bg-[#2caf88] max-md:w-20 w-20 py-4 md:px-4 text-xs ms-auto rounded text-center font-semibold'>
                           +
                           {Number(
                             value.price_change_percentage_24h
@@ -117,7 +117,7 @@ export function RateSection({ rates }: Props) {
                           %
                         </div>
                       ) : (
-                        <div className='bg-red-500 max-md:w-20 w-20 py-2 md:px-4 rounded text-xs ms-auto text-center font-semibold'>
+                        <div className='bg-red-500 max-md:w-20 w-20 py-4 md:px-4 rounded text-xs ms-auto text-center font-semibold'>
                           {Number(
                             value.price_change_percentage_24h
                           ).toLocaleString(undefined, {
